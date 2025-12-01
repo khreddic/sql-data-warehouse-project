e6c5c8a05b8aa212cfee34440cd59b2650c0e998
Kevin Reddic Jr. <khreddic@gmail.com>
2025-12-01 16:12:32 -0600

    Update ddl_bronze.sql

--- scripts/bronze_layer/ddl_bronze.sql
+++ scripts/bronze_layer/ddl_bronze.sql
@@ -12,7 +12,6 @@ Script Purpose:
 IF OBJECT_ID('bronze.crm_cust_info','U') IS NOT NULL
     DROP TABLE bronze.crm_cust_info;
 GO
-
 CREATE TABLE bronze.crm_cust_info (
     cst_ID              INT,
     cst_key             NVARCHAR(50),
@@ -27,7 +26,6 @@ GO
 IF OBJECT_ID('bronze.crm_prd_info','U') IS NOT NULL
     DROP TABLE bronze.crm_prd_info;
 GO
-
 CREATE TABLE bronze.crm_prd_info (
     prd_id              INT,
     prd_key             NVARCHAR(50),
@@ -42,7 +40,6 @@ GO
 IF OBJECT_ID('bronze.crm_sales_details','U') IS NOT NULL
     DROP TABLE bronze.crm_sales_details;
 GO
-
 CREATE TABLE bronze.crm_sales_details (
     sls_ord_num         NVARCHAR(50),
     sls_prd_key         NVARCHAR(50),
@@ -59,7 +56,6 @@ GO
 IF OBJECT_ID('bronze.erp_loc_a101','U') IS NOT NULL
     DROP TABLE bronze.erp_loc_a101;
 GO
-
 CREATE TABLE bronze.erp_loc_a101 (
     cid     NVARCHAR(50),
     cntry   NVARCHAR(50)
@@ -69,7 +65,6 @@ GO
 IF OBJECT_ID('bronze.erp_cust_az12','U') IS NOT NULL
     DROP TABLE bronze.erp_cust_az12;
 GO
-
 CREATE TABLE bronze.erp_cust_az12 (
     cid     NVARCHAR(50),
     bdate   DATE,
@@ -80,7 +75,6 @@ GO
 IF OBJECT_ID('bronze.erp_px_cat_g1v2','U') IS NOT NULL
     DROP TABLE bronze.erp_px_cat_g1v2;
 GO
-
 CREATE TABLE bronze.erp_px_cat_g1v2 (
     id          NVARCHAR(50),
     cat         NVARCHAR(50),
