d6d8b848672a788a9abec83d69aec72e001fca0c
Kevin Reddic Jr. <khreddic@gmail.com>
2025-12-01 18:39:55 -0600

    Update load_bronze_source.sql

--- scripts/bronze_layer/load_bronze_source.sql
+++ scripts/bronze_layer/load_bronze_source.sql
@@ -11,8 +11,21 @@ Script Purpose:
 */
 CREATE or ALTER  PROCEDURE bronze.load_bronze AS
 	BEGIN
+
+	PRINT '=============================='
+	PRINT '	Loading Bronze layer'
+	PRINT '=============================='
+
+	PRINT '__________________________________'
+	PRINT 'Loading CRM Tables'
+	PRINT '__________________________________'
+	
+	
+	PRINT '>> Truncate Table: bronze.crm_cust_info'
 	--TRUNCATE & LOAD CRM Source Table
 	TRUNCATE TABLE bronze.crm_cust_info;
+
+	PRINT '>> Insert Data: bronze.crm_cust_info'
 	BULK INSERT bronze.crm_cust_info
 	FROM '<INSERT FILEPATH HERE>\cust_info.csv'
 	WITH (
@@ -20,9 +33,12 @@ CREATE or ALTER  PROCEDURE bronze.load_bronze AS
 		FIELDTERMINATOR = ',',
 		TABLOCK
 	);
-
+	
+	PRINT '>> Truncate Table: bronze.crm_prd_info'
 	--TRUNCATE & LOAD CRM Source Table
 	TRUNCATE TABLE bronze.crm_prd_info;
+	
+	PRINT '>> Insert Data: bronze.crm_prd_info'
 	BULK INSERT bronze.crm_prd_info
 	FROM '<INSERT FILEPATH HERE>\prd_info.csv'
 	WITH (
@@ -30,9 +46,12 @@ CREATE or ALTER  PROCEDURE bronze.load_bronze AS
 		FIELDTERMINATOR = ',',
 		TABLOCK
 	);
-
+	
+	PRINT '>> Truncate Table: bronze.crm_sales_details'
 	--TRUNCATE & LOAD CRM Source Table
 	TRUNCATE TABLE bronze.crm_sales_details;
+	
+	PRINT '>> Insert Data: bronze.crm_sales_details'
 	BULK INSERT bronze.crm_sales_details
 	FROM '<INSERT FILEPATH HERE>\sales_details.csv'
 	WITH (
@@ -41,8 +60,16 @@ CREATE or ALTER  PROCEDURE bronze.load_bronze AS
 		TABLOCK
 	);
 
+
+	PRINT '__________________________________'
+	PRINT 'Loading ERP Tables'
+	PRINT '__________________________________'
+	
+	PRINT '>> Truncate Table: bronze.erp_cust_az12'
 	--TRUNCATE & LOAD ERP Source Table
 	TRUNCATE TABLE bronze.erp_cust_az12;
+	
+	PRINT '>> Insert Data: bronze.erp_cust_az12'
 	BULK INSERT bronze.erp_cust_az12
 	FROM '<INSERT FILEPATH HERE>\CUST_AZ12.csv'
 	WITH (
@@ -50,9 +77,12 @@ CREATE or ALTER  PROCEDURE bronze.load_bronze AS
 		FIELDTERMINATOR = ',',
 		TABLOCK
 	);
-
+	
+	PRINT '>> Truncate Table: bronze.erp_loc_a101'
 	--TRUNCATE & LOAD ERP Source Table
 	TRUNCATE TABLE bronze.erp_loc_a101;
+	
+	PRINT '>> Insert Data: bronze.erp_loc_a101'
 	BULK INSERT bronze.erp_loc_a101
 	FROM '<INSERT FILEPATH HERE>\LOC_A101.csv'
 	WITH (
@@ -60,9 +90,12 @@ CREATE or ALTER  PROCEDURE bronze.load_bronze AS
 		FIELDTERMINATOR = ',',
 		TABLOCK
 	);
-
+	
+	PRINT '>> Truncate Table: bronze.erp_px_cat_g1v2'
 	--TRUNCATE & LOAD ERP Source Table
 	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+	
+	PRINT '>> Insert Data: bronze.erp_px_cat_g1v2'
 	BULK INSERT bronze.erp_px_cat_g1v2
 	FROM '<INSERT FILEPATH HERE>\PX_CAT_G1V2.csv'
 	WITH (
